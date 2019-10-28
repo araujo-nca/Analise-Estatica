1c5f650d9adbffe85923e4e73d19667590f165c8
Nathan Araujo <nathandearaujo13@gmail.com>
2019-10-28 03:17:27 -0200

    tirar duvida para implementacao do 107 barras

--- MODELO_NAO_LINEAR_NEWTON-RAPHSON/MODELO_NAO_LINEAR_NEWTON_RAPHSON_SOMATORIO.m
+++ MODELO_NAO_LINEAR_NEWTON-RAPHSON/MODELO_NAO_LINEAR_NEWTON_RAPHSON_SOMATORIO.m
@@ -13,31 +13,30 @@ clc
 % Sistema_14_barra_2;
 % Sistema_24_barras;
 % Sistema_33_barras;
-% SISTEMA 107 BARRAS;
 
 %%  teste para implementacao
 
-% ****   Sistema teste Monticelli  ****
-
-% ------------------------------ DADOS DE BARRA -----------------------------------
-
-%  No TB G (V)     (Ang)     (Pg)    (Qg)     (Qn)    (Qm)    (Pl)   (Ql)    bshbar
-DBAR = [
-    1 3 1 1.0         .0       .00   .00     -999.9  9999.9   0.00   0.00    0.0
-    2 1 1 1.0         .0       .00   .07     -999.9  9999.9   0.30   0.00    0.0
-    ];
-
-% TB = 1: carga ; 3: referencia
-% Tipos de barra: 1 - carga (PQ), 2 - geracao (PV), 3 - referencia (V-theta)
-
-% ------------------------------ DADOS DE LINHA -----------------------------------
-
-DLIN = [
-    %FROM  TO   %R(pu)  %X(pu)   %Bsh     %TAP     %PHI                                   CH
-    1      2     0.20    1.00    0.02     1.00     .000     .000    .0     900     .0     7
-    ];
-
-PB = 1;
+% % ****   Sistema teste Monticelli  ****
+% 
+% % ------------------------------ DADOS DE BARRA -----------------------------------
+% 
+% %  No TB G (V)     (Ang)     (Pg)    (Qg)     (Qn)    (Qm)    (Pl)   (Ql)    bshbar
+% DBAR = [
+%     1 3 1 1.0         .0       .00   .00     -999.9  9999.9   0.00   0.00    0.0
+%     2 1 1 1.0         .0       .00   .07     -999.9  9999.9   0.30   0.00    0.0
+%     ];
+% 
+% % TB = 1: carga ; 3: referencia
+% % Tipos de barra: 1 - carga (PQ), 2 - geracao (PV), 3 - referencia (V-theta)
+% 
+% % ------------------------------ DADOS DE LINHA -----------------------------------
+% 
+% DLIN = [
+%     %FROM  TO   %R(pu)  %X(pu)   %Bsh     %TAP     %PHI                                   CH
+%     1      2     0.20    1.00    0.02     1.00     .000     .000    .0     900     .0     7
+%     ];
+% 
+% PB = 1;
 
 %%  declaracao de variaveis
 
